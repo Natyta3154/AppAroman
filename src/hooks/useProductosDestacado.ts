d22f842cfe5d45be77f562cdfaafce27ccc5b05f
Natyta3154 <natyrecalde86@gmail.com>
2025-11-01 17:30:07 -0300

    correcion de url desttacods

--- src/hooks/useProductosDestacado.ts
+++ src/hooks/useProductosDestacado.ts
@@ -1,4 +1,3 @@
-// src/hooks/useDestacados.ts
 import { useEffect, useState } from "react";
 import axios from "axios";
 import type { ProductoDestacadoSimple } from "../types/productoDestacado";
@@ -7,20 +6,20 @@ const API_BASE = import.meta.env.VITE_API_URL;
 
 export const useDestacados = () => {
   const [destacados, setDestacados] = useState<ProductoDestacadoSimple[]>([]);
+  console.log(destacados);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchDestacados = async () => {
+      setLoading(true);
+      setError(null);
+
+      //const categoriaId = 5; // ID de la categoría que quieras mostrar
       try {
-        setLoading(true);
-        setError(null);
-
-        const categoriaId = 5; // o la que corresponda
-const { data } = await axios.get(`${API_BASE}/api/productos/top5/${categoriaId}`, {
-          //withCredentials: true,
-        });
-
+        const { data } = await axios.get<ProductoDestacadoSimple[]>(
+          `${API_BASE}/api/productos/top5`
+        );
         setDestacados(data);
       } catch (err: any) {
         console.error(err);
